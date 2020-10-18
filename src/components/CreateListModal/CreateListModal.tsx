42402d1fd112d38b3455855db3fc44eb594398f3
Ayaki Yamaguchi <>
2020-10-18 22:35:24 +0900

    モーダルの調整

--- src/components/CreateListModal/CreateListModal.tsx
+++ src/components/CreateListModal/CreateListModal.tsx
@@ -6,33 +6,50 @@ const CreateListModal:FC = () => {
   const { globalState , setGlobalState } = useContext(Store)
   const [listTitle, setListTitle] = useState('')
   const [showing, setShowing] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  let isError = false
   const updateList = (title: string) => {
     setGlobalState({type: 'CREATE_HABIT_LIST', payload: {title: title}})
     setListTitle('')
     setShowing(false)
+  }
+  const validate = (inputValue: string) => {
+    setListTitle(inputValue)
+
+    if (inputValue === ''){
+      setErrorMessage('習慣が入力されていません')
+      isError = true
+    } else if (inputValue.length > 30) {
+      setErrorMessage('30文字以内で入力してください')
+      isError = true
+    } else {
+      isError = false
+    }
+
   }
   return(
     <div>
       { !showing &&
         <div
           className={Style.toggle_btn}
-          onClick={() => setShowing(true)}
+          onClick={()=>setShowing(true)}
         >+</div>
       }
       {
-        showing &&
-          <div className={Style.wrapper}>
+        // showing &&
+          <div className={`${Style.wrapper} ${showing ? Style.open_modal : Style.close_modal}`}>
             <h2 className={Style.title}>新しい習慣を登録する</h2>
+            { isError && <p className="error_message">{errorMessage}</p>}
             <input
-              className={Style.input_area}
+              className={`${Style.input_area} ${isError && Style.error_input_area}`}
               type="text"
               placeholder="例：読書（20ページ）"
               value={listTitle}
-              onChange={(e) => setListTitle(e.target.value)}
+              onChange={(e) => validate(e.target.value)}
             ></input>
             <div className={Style.btn_wrapper}>
               <button className={Style.submit_btn} onClick={() => updateList(listTitle)}>登録する</button>
-              <button className={Style.cancel_btn} onClick={() => setShowing(false)}>キャンセル</button>
+              <button onClick={()=>setShowing(false)} className={Style.cancel_btn}>キャンセル</button>
             </div>
           </div>
       }
