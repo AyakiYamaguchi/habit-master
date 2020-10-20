5446173ed29ba67741ce4f7584dd81153a12d25f
Ayaki Yamaguchi <>
2020-10-21 08:02:29 +0900

    Topページの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HabitList from './pages/HabitList';
-
+import TopPage from './pages/TopPage/TopPage'
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={TopPage} />
         <Route exact path="/list" component={HabitList} />
       </Switch>
     </Router>
---
+++ src/pages/TopPage/TopPage.tsx
@@ -0,0 +1,22 @@
+import React , { FC } from 'react'
+import Style from './TopPage.module.scss'
+import Logo from '../../images/logo.png'
+
+const TopPage:FC = () => {
+  return(
+    <div className={Style.wrapper}>
+      <div className={Style.background}>
+        <h1 className={Style.service_logo}><img src={Logo} className={Style.logo_img}/></h1>
+        <h2 className={Style.top_message}>JUST DO IT.</h2>
+        <div>
+          <a className={Style.signup_btn}>メールアドレスで登録</a>
+          <p className={Style.btn_between}>or</p>
+          <a className={Style.signup_btn}>Facebookアカウントで登録</a>
+          <a className={Style.login_text}>ログインはこちら</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TopPage
