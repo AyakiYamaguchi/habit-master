f57e634e9ae4c2028c2f54e63d5e4df92a248dfa
Ayaki Yamaguchi <>
2020-12-01 23:09:53 +0900

    PrivateRouteのLoginCheck処理を修正

--- src/apis/FacebookAuth.tsx
+++ src/apis/FacebookAuth.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import firebase from 'firebase';
-
-firebase.initializeApp({
-  // Authentication infomation
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
-})
-
-const provider = new firebase.auth.FacebookAuthProvider();
+import firebase , { FacebookProvider } from './FirebaseConf'
 
 export const FacebookLogin = async() => {
-  return await firebase.auth().signInWithPopup(provider).then((result)=>{
+  return await firebase.auth().signInWithPopup(FacebookProvider).then((result)=>{
     return firebase.auth().currentUser;
   }).catch((error)=>{
     return error
--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,25 +1,25 @@
 import React, { FC, useContext , useEffect , useState } from 'react'
 import { BrowserRouter as Router, Redirect } from 'react-router-dom';
-import { getCurrentUser } from '../apis/FacebookAuth'
+import firebase from '../apis/FirebaseConf'
 import { AuthContext } from '../store/Auth'
 import { SET_USER } from '../store/Auth'
+
 
 const PrivateRoute:FC = () => {
   const { AuthState, setAuthState } = useContext(AuthContext)
   const [loading, setLoading] = useState(true)
   let currentUser = AuthState.user
-  const LoginCheck = async() => {
-    console.log('実行されました')
-    await getCurrentUser().then((result)=> {
-      console.log(result)
-      // if(result){
-      //   const user = {
-      //     uid: result.uid,
-      //     userName: result.displayName
-      //   }
-      //   setAuthState({type: SET_USER, payload: {user: user}})
-      //   currentUser = user
-      // }
+  const LoginCheck = () => {
+    firebase.auth().onAuthStateChanged(function(result) {
+      if (result) {
+        const user = {
+          uid: result.uid,
+          userName: result.displayName
+        }
+        setAuthState({type: SET_USER, payload: {user: user}})
+        currentUser = user
+        console.log(user)
+      }
       setLoading(false)
     })
   }
