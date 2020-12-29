760fc65a2b43c8a4f3f1be3fe994e34bb1ce53d0
AyakiYamaguchi <ikayayaki@gmail.com>
2020-12-29 23:41:54 +0900

    cloudFunctionのテスト実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
-
+// import admin from 'firebase-admin';
+const admin = require('firebase-admin');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,3 +8,12 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp();
+
+exports.addScheduleHabits = functions
+  .region('asia-northeast1')
+  .https.onRequest(async(req,res)=>{
+    const user = admin.firestore().collection('users').get()
+    res.send({user})
+})
